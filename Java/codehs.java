aa63f7bbed2c847d73473c609eba2e676a485fb8
Jared Thompson <jtho103@learner.dvusd.org>
2022-08-18 11:49:49 -0700

    java

---
+++ Java/codehs.java
@@ -0,0 +1,14 @@
+
+
+public class codehs
+{
+   final static int max = 8;
+   
+   
+   public static void main(String args[])
+   {
+       int n = 0;
+       double d = 1.2;
+       System.out.println(max);
+   }
+}
---
+++ Java/quiz.java
@@ -0,0 +1,27 @@
+
+public class quiz
+{
+    public static void main(String[] args)
+    {
+       System.out.println("What is your favorite color?");
+       System.out.println("\tA.Blue");
+       System.out.println("\tB.Red");
+       System.out.println("\tC.Orange");
+       
+       System.out.println("What is your favorite food?");
+       System.out.println("\tA.Pizza");
+       System.out.println("\tB.Tacos");
+       System.out.println("\tC.Burgers");
+       
+       System.out.println("What is your favorite soda?");
+       System.out.println("\tA.Coke");
+       System.out.println("\tB.Pepsi");
+       System.out.println("\tC.Sprite");
+    }
+    public quiz()
+    {
+       
+    }
+
+    
+}
